42a94ab7314a2ddaa3b803a04de9e1022ff6590a
dkowalski1906 <134270198+dkowalski1906@users.noreply.github.com>
2024-10-05 20:11:58 +0200

    Lab1, Lab2, Lab3, Lab4

--- labo4-functions/src/code-fix/lab4.js
+++ labo4-functions/src/code-fix/lab4.js
@@ -1,82 +1,114 @@
 // TODO: Fix the errors in the functions below!
 
 function addTask(taskList, newTask) {
-  return newTask;
+  taskList.push(newTask);
+  return taskList;
 }
 
 function deleteTask(taskList, index) {
-  return index;
+  taskList.splice(index, 1);
+  return taskList;
 }
 
 function editTask(taskList, index, editedTask) {
-  return editedTask;
+  taskList[index] = editedTask;
+  return taskList;
 }
 
 function completeTask(taskList, index) {
-  return index;
+  taskList[index].completed = true;
+  return taskList;
 }
 
 function clearCompletedTasks(taskList) {
-  return taskList;
+  return taskList.filter((e) => e.completed === false);
 }
 
 function sortTasksByDate(taskList) {
-  return taskList;
+  const sortByDate = (a, b) => {
+    return a.date - b.date;
+  }
+
+  return taskList.sort(sortByDate);
 }
 
 function filterTasksByKeyword(taskList, keyword) {
-  return taskList;
+  const filterByKeyword = (e) => {
+    return e.toUpperCase().includes(keyword.toUpperCase());
+  }
+  return taskList.filter(filterByKeyword);
 }
 
 function getCompletedTasks(taskList) {
-  return taskList;
+  const getCompleted = (e) => {
+    return e.completed === true;
+  }
+  return taskList.filter(getCompleted);
 }
 
 function getIncompleteTasks(taskList) {
-  return taskList;
+  const getCompleted = (e) => {
+    return e.completed === false;
+  }
+  return taskList.filter(getCompleted);
 }
 
 function getTaskCount(taskList) {
-  return taskList;
+  return taskList.length;
 }
 
 function getCompletedTaskCount(taskList) {
-  return taskList;
+  const filtered = getCompletedTasks(taskList);
+  return filtered.length;
 }
 
 function getIncompleteTaskCount(taskList) {
-  return taskList;
+  const filtered = getIncompleteTasks(taskList);
+  return filtered.length;
 }
 
 function removeCompletedTasks(taskList) {
-  return taskList;
+  return taskList.filter((e) => e.completed === false);  
 }
 
 function markAllTasksAsCompleted(taskList) {
+  taskList.forEach(e => {
+    e.completed = true;
+  });
   return taskList;
 }
 
 function markAllTasksAsIncomplete(taskList) {
+  taskList.forEach(e => {
+    e.completed = false;
+  });
   return taskList;
 }
 
 function addTaskToList(taskList, task) {
-  return task;
+  taskList.push({task: task, completed: false});
+  return taskList;
 }
 
 function deleteTaskFromList(taskList, index) {
-  return index;
+  taskList.splice(index, 1);
+  return taskList;
 }
 
 function editTaskInList(taskList, index, newTask) {
-  return newTask;
+  taskList.splice(index, 1, {task : newTask, completed: false});
+  return taskList;
 }
 
 function moveTaskUp(taskList, index) {
   if (index === 0) {
     return taskList;
   }
-  // const newIndex = index - 1;
+  const newIndex = index - 1;
+  const otherElement = taskList[newIndex]
+  const currentElement = taskList[index]
+  taskList.splice(index, 1, otherElement)
+  taskList.splice(newIndex, 1, currentElement)
   return taskList;
 }
 
@@ -84,7 +116,11 @@ function moveTaskDown(taskList, index) {
   if (index === taskList.length - 1) {
     return taskList;
   }
-  // const newIndex = index + 1;
+  const newIndex = index + 1;
+  const otherElement = taskList[newIndex]
+  const currentElement = taskList[index]
+  taskList.splice(index, 1, otherElement)
+  taskList.splice(newIndex, 1, currentElement)
   return taskList;
 }
 
